c8377c5029e8c7f6b1b4a63da0f8a6b556ed49bd
ashimaashi <ashima@snakescript.com>
2022-05-18 18:01:34 +0530

    ghgh

--- wndrland/settings.py
+++ wndrland/settings.py
@@ -128,3 +128,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = "ashima@snakescript.com"
+EMAIL_HOST_PASSWORD = "ajdmwxgtnopljzpe"
+EMAIL_PORT = 587
--- wndrland_app/views.py
+++ wndrland_app/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect
 from .models import Subscriber_newsletter,contact_us
-
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string, get_template
 # Create your views here.
 def home(request):
     if request.method == 'POST':
-        email = request.POST['email']
+        email = request.POST.get('email')
         new_subscriber = Subscriber_newsletter(email=email)
         new_subscriber.save()
-        return redirect('home')
+        subject ="Subscriber"
+        message = " You are successfully subscribed"
+        from_email = settings.EMAIL_HOST_USER
+        send_mail(subject, message, from_email, [email])
+        subject1 ="New Subscriber"
+        message1 = str(email) +"New user subscribed"
+        send_mail(subject1, message1, email, [from_email])
     return render(request, 'home.html')
 
 def about(request):
@@ -18,14 +26,26 @@ def vision(request):
 
 def contact(request):
     if request.method == 'POST':
-        print(request.POST.values)
-        email = request.POST['email']
-        fname = request.POST['fname']
-        phone = request.POST['phone']
-        address = request.POST['address']
-        review = request.POST['review']
-        new_contact = contact_us(name=fname,email=email, address=address , phone=phone, review=review)
+        email = request.POST.get('email')
+        fname = request.POST.get('fname')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        review = request.POST.get('review')
+        new_contact = contact_us(name=fname,email=email, address=address, phone=phone, review=review)
         new_contact.save()
-        return redirect('contact')
+        subject ="Contact Us"
+        message = "Thankyou for contacting us"
+        from_email = settings.EMAIL_HOST_USER
+        send_mail(subject, message, from_email, [email])
+        subject1 ="Contact Us Query"
+        ctx = {
+                'email': email,
+                'fname':fname,
+                'phone':phone,
+                'address':address,
+                'review':review
+            }
+        message1 =get_template('mail.html').render(ctx)
+        send_mail(subject1, message1, email, [from_email])
 
     return render(request, 'contact.html')
